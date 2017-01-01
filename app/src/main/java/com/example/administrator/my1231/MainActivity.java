cd24b9604ab6da40d344ae982ea2ffa7c60016c2
GAM <2894919829@qq.com>
2017-01-01 17:36:18 +0800

    //user2更改

--- app/src/main/java/com/example/administrator/my1231/MainActivity.java
+++ app/src/main/java/com/example/administrator/my1231/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent=new Intent(MainActivity.this,PicActivity.class);
                 intent.putExtra("msg","图片的地址");
                 startActivity(intent);
+                //更改
             }
         });
     }
